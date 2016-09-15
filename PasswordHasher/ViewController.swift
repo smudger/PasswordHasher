3487150bb524e39f3ce586feebe85b2b35481d19
5mudg3r <smudger06@gmail.com>
2016-09-15 11:07:53 +0100

    Initial Commit

---
+++ PasswordHasher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordHasher
+//
+//  Created by Louis Smith on 15/09/2016.
+//  Copyright © 2016 Smudja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
