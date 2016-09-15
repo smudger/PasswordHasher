d7da9ef16e9dc5bae0b998a7f332ccbed5dbaa0c
5mudg3r <smudger06@gmail.com>
2016-09-15 12:03:14 +0100

    initial commit

--- PasswordHasher/ViewController.swift
+++ PasswordHasher/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hashStatusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onButtonPress(sender: UIButton) {
+        let title = sender.titleForState(.Normal)!
+        
+        hashStatusLabel.text = "\(title)ed"
+        
+    }
 
 }
 
